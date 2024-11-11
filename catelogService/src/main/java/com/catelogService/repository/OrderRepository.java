f7b91b89e404bd1d4190460e3600f3b52c6183a9
Prathamesh Gurav <pg.officework@gmail.com>
2024-11-11 17:12:12 +0530

    adding Catalog and User service modules

---
+++ catelogService/src/main/java/com/catelogService/repository/OrderRepository.java
@@ -0,0 +1,15 @@
+package com.catelogService.repository;
+
+import com.catelogService.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Integer> {
+
+    List<Order> findByCategory(String category);
+
+
+}
