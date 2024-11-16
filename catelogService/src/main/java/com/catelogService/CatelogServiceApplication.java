cc101111e1674c40e96ad538b9ccfd3001ef481e
Prathamesh Gurav <pg.officework@gmail.com>
2024-11-16 11:57:38 +0530

    fix
    successfully fixed

--- catelogService/src/main/java/com/catelogService/CatelogServiceApplication.java
+++ catelogService/src/main/java/com/catelogService/CatelogServiceApplication.java
@@ -5,6 +5,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
